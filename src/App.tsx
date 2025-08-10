8e8325004ca94c81de5020e23e795f0290395ec8
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-09 22:14:10 -0300

    feat: agregado css bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,27 @@ const App = () => {
     console.log(nombre + " " + color + " " + condiciones)
   }
   return (
-    <>
+    <div className="container mx-auto mt-3">
       <h1>Formulario controlado de clientes</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Ingrese su nombre completo" value={nombre} onChange={(e)=>setNombre(e.target.value)}/>
-        <h2>{nombre}</h2>
-        <br />
-        <select defaultValue="" value={color} onChange={(e)=>setColor(e.target.value)}>
-          <option value="" disabled>Seleccione un color</option>
-          <option value="rojo">Rojo</option>
-          <option value="azul">Azul</option>
-          <option value="verde">Verde</option>
-        </select>
-        <h2>{color}</h2>
-        <br />
-        <input type="checkbox" checked={condiciones} onChange={(e)=>setCondiciones(e.target.checked)}/>
-        <span>Aceptar las condiciones</span>
-        <h2>{condiciones}</h2>
-        <br />
-        <button type="submit">Enviar</button>
+        <div className="mb-3">
+          <input type="text" placeholder="Ingrese su nombre completo" className="form-control" value={nombre} onChange={(e)=>setNombre(e.target.value)}/>
+        </div>
+        <div className="mb-3">
+          <select defaultValue="" className="form-select" value={color} onChange={(e)=>setColor(e.target. value)}>
+            <option value="" disabled>Seleccione un color</option>
+            <option value="rojo">Rojo</option>
+            <option value="azul">Azul</option>
+            <option value="verde">Verde</option>
+          </select>
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" checked={condiciones} onChange={(e)=>setCondiciones(e.target.checked)}/>
+          <span className="form-check-label">Aceptar las condiciones</span>
+        </div>
+        <button type="submit" className="btn btn-primary">Enviar</button>
       </form>
-    </>
+    </div>
   )
 }
 export default App
